184211dc9226f3b68efe03534ba43e7357c5f21a
xqt19 <56241735+xqt19@users.noreply.github.com>
2023-08-28 18:05:39 +0800

    helloworld app

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,70 @@
+import com.sun.javafx.image.IntPixelGetter;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import sun.reflect.generics.tree.Tree;
+
+import java.text.*;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.*;
+import java.util.stream.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//1.08 --> 30
+
+
+
+
+
+
+
+
+
+
+
+
+        String str = "aaeiounymphAAEIOU";
+        String str2 = str.replace("[aeiouAEIOU]","!");
+        System.out.println(str2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
